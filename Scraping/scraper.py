3319015962c9f5e5aa1c92cf6330b1e763308c9a
the-rileyj <rileyjohnson99@hotmail.com>
2018-02-12 09:00:34 -0600

    Basically finished setup

--- Scraping/scraper.py
+++ Scraping/scraper.py
@@ -1,7 +1,13 @@
 from splinter import Browser
+import glob, json
 
 def main():
-    pass
+    with open(glob.glob("*_config.json")) as fi:
+        config = json.load(fi)
+    executable_path = {'executable_path': config['browser']}
+    b = Browser(config['type'], headless=config['headless'], **executable_path)
+    
+    
 
 if __name__ == "__main__":
     main()
