202bc88cdc9b432bfc5ceb69eee4a0d526de8293
Collin Rumpca <collin.rumpca@trojans.dsu.edu>
2018-02-14 21:40:27 -0600

    Renamed Form1 to login

--- DSUScheduleBuilder/Program.cs
+++ DSUScheduleBuilder/Program.cs
@@ -15,7 +15,7 @@ namespace DSUScheduleBuilder
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
