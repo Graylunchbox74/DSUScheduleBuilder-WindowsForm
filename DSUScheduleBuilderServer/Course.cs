975fdf948743643a513f2dcbb034bf04d3b26e6c
Collin Rumpca <collin.rumpca@trojans.dsu.edu>
2018-02-16 10:51:02 -0600

    Added Course and Student classes

---
+++ DSUScheduleBuilderServer/Course.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSUScheduleBuilderServer
+{
+    class Course
+    {
+        private string _courseName;
+        public string CourseName
+        {
+            get { return _courseName; }
+        }
+
+        private string _courseCode;
+        public string CourseCode
+        {
+            get { return _courseCode; }
+        }
+
+        private string _professor;
+        public string Professor
+        {
+            get { return _professor; }
+        }
+
+        private int _startTime;
+        public int StartTime
+        {
+            get { return _startTime; }
+        }
+
+        private int _endTime;
+        public int EndTime
+        {
+            get { return _endTime; }
+        }
+
+        private string _location;
+        public string Location
+        {
+            get { return _location; }
+        }
+
+
+        private int _credits;
+        public int Credits
+        {
+            get { return _credits; }
+        }
+
+        private Course _lab;
+
+        private string _courseDescription;
+        public string CourseDescription
+        {
+            get { return _courseDescription; }
+        }
+
+    }
+}
---
+++ DSUScheduleBuilderServer/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSUScheduleBuilderServer
+{
+    class Student
+    {
+        private string _name;
+        public  string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        private List<Course> _plannedClasses;
+        private List<Course> _takenClasses;
+        private string _major;
+        private int _catalogYear;
+    }
+}
