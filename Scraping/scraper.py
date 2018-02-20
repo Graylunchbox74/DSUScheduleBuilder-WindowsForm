5c57f5f16718fa196bfa0773b06c5e49b50bd64a
the-rileyj <rileyjohnson99@hotmail.com>
2018-02-20 14:10:51 -0600

    Reduced redundancy in scraper code

--- Scraping/scraper.py
+++ Scraping/scraper.py
@@ -1,13 +1,22 @@
 from splinter import Browser
+from time import sleep
 import glob, json
 
+with open(glob.glob("*_config.json")[0]) as fi:
+    config = json.load(fi)
+executable_path = {'executable_path': config['browser']}
+b = Browser(config['type'], headless=config['headless'], **executable_path)
+
 def main():
-    with open(glob.glob("*_config.json")) as fi:
-        config = json.load(fi)
-    executable_path = {'executable_path': config['browser']}
-    b = Browser(config['type'], headless=config['headless'], **executable_path)
+    getToQuery()
+    b.quit()
     
-    
+def getToQuery():
+    b.visit("https://wa-dsu.prod.sdbor.edu/WebAdvisor/webadvisor")
+    b.find_by_text(" Prospective Students").click()
+    b.find_by_text("Search for Class Sections").click()
+    sleep(5)
+
 
 if __name__ == "__main__":
     main()
