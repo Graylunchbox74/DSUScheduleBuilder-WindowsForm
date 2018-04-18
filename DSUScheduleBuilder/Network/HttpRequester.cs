2c4e0bb95bc6c31a6339a1077f67c370d9d773db
Brendan Hansen <brendan.f.hansen@gmail.com>
2018-04-18 13:38:26 -0500

    Started working on C# networking

---
+++ DSUScheduleBuilder/Network/HttpRequester.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace DSUScheduleBuilder.Network {
+    class UserResponse {
+        public int? uid { get; set; }
+        public string name { get; set; }
+        public string major { get; set; }
+
+        public int? errorCode { get; set; }
+        public string errorMessage { get; set; }
+        public string errorLocation { get; set; }
+    }
+
+    class HttpRequester {
+        public static void getUser(string name) {
+            var rc = new RestClient("http://localhost:4200");
+            var getRequest = new RestRequest(Method.GET);
+            getRequest.Resource = "user/" + name;
+
+            var response = rc.Execute<UserResponse>(getRequest);
+            UserResponse user = response.Data;
+            if (user.errorCode != null) {
+                Console.WriteLine("ERROR");
+            } else {
+                Console.WriteLine(response.Data.name, response.Data.major);
+            }
+        }
+    }
+}
--- DSUScheduleBuilder/Program.cs
+++ DSUScheduleBuilder/Program.cs
@@ -13,6 +13,8 @@ namespace DSUScheduleBuilder
         [STAThread]
         static void Main()
         {
+            Network.HttpRequester.getUser("test1");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
