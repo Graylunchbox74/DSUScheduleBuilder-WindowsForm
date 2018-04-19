34cfdf6786cb58704944494aa232bf78aa7e2a12
Brendan Hansen <brendan.f.hansen@gmail.com>
2018-04-19 14:43:55 -0500

    Working on learning RestSharp

--- DSUScheduleBuilder/Network/HttpRequester.cs
+++ DSUScheduleBuilder/Network/HttpRequester.cs
@@ -6,28 +6,39 @@ using System.Threading.Tasks;
 using RestSharp;
 
 namespace DSUScheduleBuilder.Network {
-    class UserResponse {
-        public int? uid { get; set; }
-        public string name { get; set; }
-        public string major { get; set; }
-
+    class Errorable {
         public int? errorCode { get; set; }
         public string errorMessage { get; set; }
     }
 
+    class UserResponse : Errorable {
+        public int? uid { get; set; }
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string majors { get; set; }
+        public string minors { get; set; }
+        public string email { get; set; }
+    }
+
+
+
     class HttpRequester {
-        public static void getUser(string name) {
+        public static void getUser(string uuid) {
             var rc = new RestClient("http://localhost:4200");
             var getRequest = new RestRequest(Method.GET);
-            getRequest.Resource = "api/user/" + name;
+            getRequest.Resource = "api/user/" + uuid;
 
             var response = rc.Execute<UserResponse>(getRequest);
             UserResponse user = response.Data;
             if (user.errorCode != null) {
                 Console.WriteLine("ERROR");
             } else {
-                Console.WriteLine(response.Data.name, response.Data.major);
+                Console.WriteLine(response.Data.lname);
             }
+        }
+
+        public static void getPreviousClasses(string uuid) {
+
         }
     }
 }
