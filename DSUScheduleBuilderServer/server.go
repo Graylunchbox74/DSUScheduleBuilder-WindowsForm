2b64f6f776910c8afa4efea4b021f58d5fccb5d6
Graylunchbox74 <collin.rumpca@trojans.dsu.edu>
2018-04-20 23:13:32 -0500

    added functions into collin.go

--- DSUScheduleBuilderServer/server.go
+++ DSUScheduleBuilderServer/server.go
@@ -257,15 +257,43 @@ func updateEnrolledClass(class course, keyword, newValue string) (int, error) {
 	return 500, err
 }
 
-func getEnrolledClass(uid int, classID string) (course, int, error) {
+func getEnrolledClasses(uid int) ([]course, int, error) {
+	location := "getEnrolledClasses"
+	var classes []course
 	var class course
-	location := "getEnrolledClass"
-	err := db.QueryRow("SELECT FROM EnrolledClasses WHERE userID=$1 AND classID=$2", uid, classID).Scan(&class.UserID, &class.ClassID, &class.ClassName, &class.Teacher, &class.Location, &class.StartTime, &class.EndTime, &class.StartDate, &class.EndDate, &class.Credits)
-	checkLogError(location, "Selecting from the enrolledClasses table", err)
-	if err == nil {
-		return class, 200, err
+	parameter := "|" + strconv.Itoa(uid) + "|"
+	rows, err := db.Query(`
+		SELECT *
+		FROM EnrolledClasses 
+		WHERE userID LIKE $1`, parameter,
+	)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		class = course{}
+
+		err = rows.Scan(
+			&class.UserID, &class.ClassID, &class.ClassName, &class.Teacher,
+			&class.StartTime, &class.EndTime, &class.StartDate, &class.EndDate, &class.Credits,
+		)
+
+		if err != nil {
+			go logError(location, "1", err)
+			return classes, 5, err
+		}
+
+		classes = append(classes, class)
 	}
-	return class, 500, err
+
+	err = rows.Err()
+
+	if err != nil {
+		go logError(location, "2", err)
+		return classes, 5, err
+	}
+
+	return classes, 200, nil
 }
 
 //previous class database functions
@@ -527,6 +555,35 @@ func main() {
 
 				var classes []course
 				classes, errno, err := getPreviousClasses(userID)
+
+				if err != nil {
+					currentError := createErrorStruct(errno, c.Request.URL.String(), "2", err)
+					c.JSON(500, currentError)
+					return
+				}
+
+				c.JSON(200, classes)
+			})
+
+			courses.GET("/enrolled/:uuid", func(c *gin.Context) {
+				uuid := c.Param("uuid")
+				var userID int
+				err := db.QueryRow("SELECT uid FROM USER_SESSIONS WHERE uuid=$1", uuid).Scan(&userID)
+
+				if err == sql.ErrNoRows {
+					currentError := createErrorStruct(2, c.Request.URL.String(), "3", err)
+					c.JSON(500, currentError)
+					return
+				}
+
+				if err != nil {
+					currentError := createErrorStruct(3, c.Request.URL.String(), "1", err)
+					c.JSON(500, currentError)
+					return
+				}
+
+				var classes []course
+				classes, errno, err := getEnrolledClasses(userID)
 
 				if err != nil {
 					currentError := createErrorStruct(errno, c.Request.URL.String(), "2", err)
