65b8b2ce1a67c1138d4c49ac2da7d6129ee0a2ee
Brendan Hansen <brendan.f.hansen@gmail.com>
2018-04-21 01:12:52 -0500

    Added login functionality to the forms

--- DSUScheduleBuilder/MainMenu.cs
+++ DSUScheduleBuilder/MainMenu.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 namespace DSUScheduleBuilder
 {
     using Network;
+    using Models;
 
     public partial class MainMenu : Form
     {
@@ -20,7 +21,13 @@ namespace DSUScheduleBuilder
 
         private void MainMenu_Load(object sender, EventArgs e)
         {
-
+            List<Course> courses = HttpRequester.Default.GetEnrolledCourses();
+            foreach (Course c in courses)
+            {
+                Console.WriteLine(c.ClassID);
+                Console.WriteLine(c.ClassName);
+                Console.WriteLine(c.Teacher);
+            }
         }
 
         private void CurrentBtn_Click(object sender, EventArgs e)
@@ -50,6 +57,7 @@ namespace DSUScheduleBuilder
         }
 
         private void MainMenu_FormClosed(object sender, FormClosedEventArgs e) {
+            HttpRequester.Default.Logout();
             Application.Exit();
         }
 
