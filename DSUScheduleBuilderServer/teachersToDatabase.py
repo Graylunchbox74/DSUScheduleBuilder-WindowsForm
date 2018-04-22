84ef9ace2e0795bce0fa04795443f978459e9069
Brendan Hansen <brendan.f.hansen@gmail.com>
2018-04-22 00:52:11 -0500

    Made converting things work

--- DSUScheduleBuilderServer/teachersToDatabase.py
+++ DSUScheduleBuilderServer/teachersToDatabase.py
@@ -8,20 +8,19 @@ data = None
 with open("../Scraping/2018FA.json", "r") as f:
     data = json.load(f)
 
-for collegeKey, college in data[term].items():
-    print (collegeKey)
-    for couresName, course in college.items():
-        for section in course:
-            for sectionID, cd in section.items():
-                cmd = '''INSERT INTO
-                teachers
-                ( name, email, phone)
-                VALUES (?, ?, ?)
-                '''
+for teacherEmail, teacher in data["Teachers"].items():
+    print (teacherEmail)
+    cmd = '''INSERT INTO
+        teachers
+        ( name, email, phone )
+        VALUES (?, ?, ?)
+        '''
 
-                param = ( cd["name"],
-                    cd["email"],
-                    cd["phone"] )
-                db.execute(cmd, param)
+    param = (
+        teacher["Name"],
+        teacher["Email"],
+        teacher["Phone"]
+    )
+    db.execute(cmd, param)
 
 db.commit()
