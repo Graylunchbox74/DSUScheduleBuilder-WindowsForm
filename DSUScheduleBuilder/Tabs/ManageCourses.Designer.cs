72f8dfa6dadb20157e190599de60cfce4ec7ba90
Thomas Lange <Thomas.Lange@trojans.dsu.edu>
2018-04-30 23:14:56 -0500

    Changed Textbox colors to Dim Grey to make cursor visible again.

--- DSUScheduleBuilder/Tabs/ManageCourses.Designer.cs
+++ DSUScheduleBuilder/Tabs/ManageCourses.Designer.cs
@@ -46,10 +46,9 @@
             // ViewPreviousBtn
             // 
             this.ViewPreviousBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ViewPreviousBtn.Location = new System.Drawing.Point(666, 520);
-            this.ViewPreviousBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.ViewPreviousBtn.Location = new System.Drawing.Point(999, 800);
             this.ViewPreviousBtn.Name = "ViewPreviousBtn";
-            this.ViewPreviousBtn.Size = new System.Drawing.Size(149, 52);
+            this.ViewPreviousBtn.Size = new System.Drawing.Size(224, 80);
             this.ViewPreviousBtn.TabIndex = 21;
             this.ViewPreviousBtn.Text = "View Taken";
             this.ViewPreviousBtn.UseVisualStyleBackColor = true;
@@ -58,10 +57,9 @@
             // ViewEnrolledBtn
             // 
             this.ViewEnrolledBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ViewEnrolledBtn.Location = new System.Drawing.Point(470, 520);
-            this.ViewEnrolledBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.ViewEnrolledBtn.Location = new System.Drawing.Point(705, 800);
             this.ViewEnrolledBtn.Name = "ViewEnrolledBtn";
-            this.ViewEnrolledBtn.Size = new System.Drawing.Size(149, 52);
+            this.ViewEnrolledBtn.Size = new System.Drawing.Size(224, 80);
             this.ViewEnrolledBtn.TabIndex = 20;
             this.ViewEnrolledBtn.Text = "View Enrolled";
             this.ViewEnrolledBtn.UseVisualStyleBackColor = true;
@@ -69,20 +67,22 @@
             // 
             // PreviousCourseIdTxt
             // 
-            this.PreviousCourseIdTxt.BackColor = System.Drawing.Color.Gray;
+            this.PreviousCourseIdTxt.BackColor = System.Drawing.Color.DimGray;
             this.PreviousCourseIdTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.PreviousCourseIdTxt.ForeColor = System.Drawing.Color.White;
-            this.PreviousCourseIdTxt.Location = new System.Drawing.Point(250, 80);
+            this.PreviousCourseIdTxt.Location = new System.Drawing.Point(375, 123);
+            this.PreviousCourseIdTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PreviousCourseIdTxt.Name = "PreviousCourseIdTxt";
-            this.PreviousCourseIdTxt.Size = new System.Drawing.Size(131, 20);
+            this.PreviousCourseIdTxt.Size = new System.Drawing.Size(196, 26);
             this.PreviousCourseIdTxt.TabIndex = 10;
             // 
             // AddPreviousCourseBtn
             // 
             this.AddPreviousCourseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.AddPreviousCourseBtn.Location = new System.Drawing.Point(89, 195);
+            this.AddPreviousCourseBtn.Location = new System.Drawing.Point(134, 300);
+            this.AddPreviousCourseBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.AddPreviousCourseBtn.Name = "AddPreviousCourseBtn";
-            this.AddPreviousCourseBtn.Size = new System.Drawing.Size(216, 35);
+            this.AddPreviousCourseBtn.Size = new System.Drawing.Size(324, 54);
             this.AddPreviousCourseBtn.TabIndex = 13;
             this.AddPreviousCourseBtn.Text = "Add Previous Course";
             this.AddPreviousCourseBtn.UseVisualStyleBackColor = true;
@@ -92,9 +92,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label2.Location = new System.Drawing.Point(28, 79);
+            this.label2.Location = new System.Drawing.Point(42, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(216, 18);
+            this.label2.Size = new System.Drawing.Size(317, 26);
             this.label2.TabIndex = 23;
             this.label2.Text = "Previous Course ID (CSC-105):";
             // 
@@ -102,29 +103,32 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 37);
+            this.label1.Location = new System.Drawing.Point(4, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(205, 24);
+            this.label1.Size = new System.Drawing.Size(313, 33);
             this.label1.TabIndex = 22;
             this.label1.Text = "Add a previous course:";
             // 
             // CourseNameTxt
             // 
-            this.CourseNameTxt.BackColor = System.Drawing.Color.Gray;
+            this.CourseNameTxt.BackColor = System.Drawing.Color.DimGray;
             this.CourseNameTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.CourseNameTxt.ForeColor = System.Drawing.Color.White;
-            this.CourseNameTxt.Location = new System.Drawing.Point(250, 113);
+            this.CourseNameTxt.Location = new System.Drawing.Point(375, 174);
+            this.CourseNameTxt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.CourseNameTxt.Name = "CourseNameTxt";
-            this.CourseNameTxt.Size = new System.Drawing.Size(131, 20);
+            this.CourseNameTxt.Size = new System.Drawing.Size(196, 26);
             this.CourseNameTxt.TabIndex = 11;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label3.Location = new System.Drawing.Point(4, 113);
+            this.label3.Location = new System.Drawing.Point(6, 174);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(240, 18);
+            this.label3.Size = new System.Drawing.Size(349, 26);
             this.label3.TabIndex = 26;
             this.label3.Text = "Course name (Intro to Computers):";
             // 
@@ -132,25 +136,27 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.label4.Location = new System.Drawing.Point(111, 151);
+            this.label4.Location = new System.Drawing.Point(166, 232);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(133, 18);
+            this.label4.Size = new System.Drawing.Size(195, 26);
             this.label4.TabIndex = 28;
             this.label4.Text = "Number of Credits:";
             // 
             // CreditsUpDown
             // 
-            this.CreditsUpDown.BackColor = System.Drawing.Color.Gray;
+            this.CreditsUpDown.BackColor = System.Drawing.Color.DimGray;
             this.CreditsUpDown.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.CreditsUpDown.ForeColor = System.Drawing.Color.White;
-            this.CreditsUpDown.Location = new System.Drawing.Point(250, 149);
+            this.CreditsUpDown.Location = new System.Drawing.Point(375, 229);
+            this.CreditsUpDown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.CreditsUpDown.Maximum = new decimal(new int[] {
             60,
             0,
             0,
             0});
             this.CreditsUpDown.Name = "CreditsUpDown";
-            this.CreditsUpDown.Size = new System.Drawing.Size(131, 20);
+            this.CreditsUpDown.Size = new System.Drawing.Size(196, 26);
             this.CreditsUpDown.TabIndex = 12;
             this.CreditsUpDown.Value = new decimal(new int[] {
             3,
@@ -160,9 +166,10 @@
             // 
             // PreviousCourseView
             // 
-            this.PreviousCourseView.Location = new System.Drawing.Point(414, 37);
+            this.PreviousCourseView.Location = new System.Drawing.Point(621, 57);
+            this.PreviousCourseView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PreviousCourseView.Name = "PreviousCourseView";
-            this.PreviousCourseView.Size = new System.Drawing.Size(455, 457);
+            this.PreviousCourseView.Size = new System.Drawing.Size(682, 703);
             this.PreviousCourseView.TabIndex = 21;
             this.PreviousCourseView.Text = "PreviousCourseView";
             this.PreviousCourseView.Visible = false;
@@ -170,16 +177,17 @@
             // 
             // EnrolledCourseView
             // 
-            this.EnrolledCourseView.Location = new System.Drawing.Point(414, 37);
+            this.EnrolledCourseView.Location = new System.Drawing.Point(621, 57);
+            this.EnrolledCourseView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.EnrolledCourseView.Name = "EnrolledCourseView";
-            this.EnrolledCourseView.Size = new System.Drawing.Size(455, 457);
+            this.EnrolledCourseView.Size = new System.Drawing.Size(682, 703);
             this.EnrolledCourseView.TabIndex = 20;
             this.EnrolledCourseView.Text = "EnrolledCourseView";
             this.EnrolledCourseView.Click += new System.EventHandler(this.EnrolledCourseView_Click);
             // 
             // ManageCourses
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.CreditsUpDown);
             this.Controls.Add(this.label4);
@@ -193,8 +201,9 @@
             this.Controls.Add(this.EnrolledCourseView);
             this.Controls.Add(this.ViewPreviousBtn);
             this.Controls.Add(this.ViewEnrolledBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ManageCourses";
-            this.Size = new System.Drawing.Size(872, 606);
+            this.Size = new System.Drawing.Size(1308, 932);
             ((System.ComponentModel.ISupportInitialize)(this.CreditsUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
