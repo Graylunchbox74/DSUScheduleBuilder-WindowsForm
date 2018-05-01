f6f4130f7601e418be8d2946fca38893b73b6036
Riley <rileyjohnson99@hotmail.com>
2018-05-01 11:27:56 -0500

    Consolidated the JSON to DB converters into single file, new db file

--- DSUScheduleBuilderServer/convertJSONtoDB.py
+++ DSUScheduleBuilderServer/convertJSONtoDB.py
@@ -1,97 +1,103 @@
 import sqlite3
 import json
-import sys
 
 db = sqlite3.connect("userDatabase.db")
 
 data = None
-with open("../Scraping/2018FA.json", "r") as f:
+with open("../Scraping/totalData.json", "r") as f:
     data = json.load(f)
 
-if len(sys.argv) < 2:
-    print("Please provide a term to use")
-    sys.exit(1)
+for term in data:
+    if term != "Teachers":
+        for collegeKey, college in data[term].items():
+            print (collegeKey)
+            for couresName, course in college.items():
+                for section in course:
+                    for sectionID, cd in section.items():
+                        cmd = '''INSERT INTO
+                        availableCourses
+                        ( sectionID
+                        , open
+                        , academicLevel
+                        , courseID
+                        , description
+                        , courseName
+                        , startDate
+                        , endDate
+                        , location
+                        , meetingInformation
+                        , supplies
+                        , credits
+                        , slotsAvailable
+                        , slotsCapacity
+                        , slotsWaitlist
+                        , timeStart
+                        , timeEnd
+                        , professorEmails
+                        , prereqNonCourse
+                        , recConcurrentCourses
+                        , reqConcurrentCourses
+                        , prereqCoursesAnd
+                        , prereqCoursesOR
+                        , instructionalMethods
+                        , daysOfWeek
+                        , term)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                        '''
 
-term = sys.argv[1]
-if data[term] == None:
-    print("Invalid term")
-    sys.exit(1)
+                        daysOfWeek = []
+                        daysConvert = {
+                            "Monday": "mon",
+                            "Tuesday": "tues",
+                            "Wednesday": "wed",
+                            "Thursday": "thur",
+                            "Friday": "fri"
+                        }
+                        for day, shortName in daysConvert.items():
+                            if day in cd["MeetingInformation"]:
+                                daysOfWeek.append(shortName)
+                        
 
-for collegeKey, college in data[term].items():
-    print (collegeKey)
-    for couresName, course in college.items():
-        for section in course:
-            for sectionID, cd in section.items():
-                cmd = '''INSERT INTO
-                availableCourses
-                ( sectionID
-                , open
-                , academicLevel
-                , courseID
-                , description
-                , courseName
-                , startDate
-                , endDate
-                , location
-                , meetingInformation
-                , supplies
-                , credits
-                , slotsAvailable
-                , slotsCapacity
-                , slotsWaitlist
-                , timeStart
-                , timeEnd
-                , professorEmails
-                , prereqNonCourse
-                , recConcurrentCourses
-                , reqConcurrentCourses
-                , prereqCoursesAnd
-                , prereqCoursesOR
-                , instructionalMethods
-                , daysOfWeek
-                , term)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                        param = (sectionID,
+                            cd["Open"],
+                            cd["AcademicLevel"],
+                            cd["CourseCode"],
+                            cd["CourseDescription"],
+                            cd["CourseName"],
+                            cd["DateStart"],
+                            cd["DateEnd"],
+                            cd["Location"],
+                            cd["MeetingInformation"], 
+                            cd["Supplies"],
+                            cd["Credits"],
+                            cd["SlotsAvailable"],
+                            cd["SlotsCapacity"],
+                            cd["SlotsWaitlist"],
+                            cd["TimeStart"],
+                            cd["TimeEnd"],
+                            "|" + ("|".join(cd["ProfessorEmails"])) + "|",
+                            cd["PrereqNonCourse"],
+                            "|" + "|".join(cd["RecConcurrentCourses"]) + "|",
+                            "|" + "|".join(cd["ReqConcurrentCourses"]) + "|",
+                            "|" + "|".join(cd["PrereqCourses"]["and"]) + "|",
+                            "|" + "|".join(cd["PrereqCourses"]["or"]) + "|",
+                            "",
+                            "|" + "|".join(daysOfWeek) + "|",
+                            term)
+                        db.execute(cmd, param)
+    else:
+        for teacherEmail, teacher in data[term].items():
+            print (teacherEmail)
+            cmd = '''INSERT INTO
+                teachers
+                ( name, email, phone )
+                VALUES (?, ?, ?)
                 '''
 
-                daysOfWeek = []
-                daysConvert = {
-                    "Monday": "mon",
-                    "Tuesday": "tues",
-                    "Wednesday": "wed",
-                    "Thursday": "thur",
-                    "Friday": "fri"
-                }
-                for day, shortName in daysConvert.items():
-                    if day in cd["MeetingInformation"]:
-                        daysOfWeek.append(shortName)
-                
-
-                param = (sectionID,
-                    cd["Open"],
-                    cd["AcademicLevel"],
-                    cd["CourseCode"],
-                    cd["CourseDescription"],
-                    cd["CourseName"],
-                    cd["DateStart"],
-                    cd["DateEnd"],
-                    cd["Location"],
-                    cd["MeetingInformation"], 
-                    cd["Supplies"],
-                    cd["Credits"],
-                    cd["SlotsAvailable"],
-                    cd["SlotsCapacity"],
-                    cd["SlotsWaitlist"],
-                    cd["TimeStart"],
-                    cd["TimeEnd"],
-                    "|" + ("|".join(cd["ProfessorEmails"])) + "|",
-                    cd["PrereqNonCourse"],
-                    "|" + "|".join(cd["RecConcurrentCourses"]) + "|",
-                    "|" + "|".join(cd["ReqConcurrentCourses"]) + "|",
-                    "|" + "|".join(cd["PrereqCourses"]["and"]) + "|",
-                    "|" + "|".join(cd["PrereqCourses"]["or"]) + "|",
-                    "",
-                    "|" + "|".join(daysOfWeek) + "|",
-                    term)
-                db.execute(cmd, param)
-
+            param = (
+                teacher["Name"],
+                teacher["Email"],
+                teacher["Phone"]
+            )
+            db.execute(cmd, param)
 db.commit()
